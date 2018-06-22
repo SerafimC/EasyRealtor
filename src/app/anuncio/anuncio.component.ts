34836990cb60aae03d12f6ef5da15719129aa6d1
mauricioandrek@hotmail.com <mauricioandrek@hotmail.com>
2018-06-21 22:53:03 -0300

    - Adicionado Formulário a pagina de anuncio

--- src/app/anuncio/anuncio.component.ts
+++ src/app/anuncio/anuncio.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-anuncio',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./anuncio.component.css']
 })
 export class AnuncioComponent implements OnInit {
+  tipos = [
+    {value: 'alugar-0', viewValue: 'Alugar'},
+    {value: 'comprar-1', viewValue: 'Comprar'}
+  ];
+  cidades = [
+    {value: 'chapeco-0', viewValue: 'Chapecó'},
+    {value: 'xaxim-1', viewValue: 'Xaxim'},
+    {value: 'xanxere-2', viewValue: 'Xanxerê'},
+    {value: 'seara-3', viewValue: 'Seara'},
+    {value: 'concordia-4', viewValue: 'Concórdia'}
+  ];
+  email = new FormControl('', [Validators.required, Validators.email]);
+  tipo = new FormControl('', [Validators.required]);
+  cidade = new FormControl('', [Validators.required]);
+  descricao = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  getErrorEmailMessage() {
+    return this.email.hasError('required') ? 'Você precisa informar algo' :
+        this.email.hasError('email') ? 'Não é um e-mail válido' :
+            '';
+  }
+  getErrorTipoMessage() {
+    return this.email.hasError('required') ? 'Você precisa informar o tipo' :
+            '';
+  }
+  getErrorCidadeMessage() {
+    return this.email.hasError('required') ? 'Você precisa informar a cidade' :
+            '';
+  }
+  getErrorDescricaoMessage() {
+    return this.email.hasError('required') ? 'Você precisa informar a descrição' :
+            '';
+  }
   ngOnInit() {
   }
-
+  cancelar() {
+    this.router.navigate(['/login']);
+  }
+  confirmar() {
+    if (!this.email.hasError('required') && !this.email.hasError('email')) {
+      if (!this.tipo.hasError('required')) {
+        if (!this.cidade.hasError('required')) {
+          if (!this.descricao.hasError('required')) {
+            alert('Seu interesse foi salvo, logo entraremos em contato!');
+          }
+        }
+      }
+    }
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,46 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatGridListModule} from '@angular/material/grid-list';
-import {MatButtonModule} from '@angular/material/button';
-import {MatDividerModule} from '@angular/material/divider';
-import {MatListModule} from '@angular/material/list';
-
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatAutocompleteModule,
+  MatBadgeModule,
+  MatBottomSheetModule,
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatCardModule,
+  MatCheckboxModule,
+  MatChipsModule,
+  MatDatepickerModule,
+  MatDialogModule,
+  MatDividerModule,
+  MatExpansionModule,
+  MatGridListModule,
+  MatIconModule,
+  MatInputModule,
+  MatListModule,
+  MatMenuModule,
+  MatNativeDateModule,
+  MatPaginatorModule,
+  MatProgressBarModule,
+  MatProgressSpinnerModule,
+  MatRadioModule,
+  MatRippleModule,
+  MatSelectModule,
+  MatSidenavModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatSnackBarModule,
+  MatSortModule,
+  MatStepperModule,
+  MatTableModule,
+  MatTabsModule,
+  MatToolbarModule,
+  MatTooltipModule,
+  MatTreeModule,
+} from '@angular/material';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -24,7 +58,45 @@ import { AppRoutingModule } from './app-routing.module';
     AlugarComponent,
     AnuncioComponent
   ],
+  exports: [
+    MatAutocompleteModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatCheckboxModule,
+    MatChipsModule,
+    MatStepperModule,
+    MatDatepickerModule,
+    MatDialogModule,
+    MatDividerModule,
+    MatExpansionModule,
+    MatGridListModule,
+    MatIconModule,
+    MatInputModule,
+    MatListModule,
+    MatMenuModule,
+    MatNativeDateModule,
+    MatPaginatorModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    MatRadioModule,
+    MatRippleModule,
+    MatSelectModule,
+    MatSidenavModule,
+    MatSliderModule,
+    MatSlideToggleModule,
+    MatSnackBarModule,
+    MatSortModule,
+    MatTableModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatTooltipModule,
+    MatTreeModule,
+  ],
   imports: [
+    MatNativeDateModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
@@ -32,7 +104,13 @@ import { AppRoutingModule } from './app-routing.module';
     MatGridListModule,
     MatButtonModule,
     MatDividerModule,
-    MatListModule
+    MatListModule,
+    MatSelectModule,
+    MatAutocompleteModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
